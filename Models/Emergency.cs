ebdf8314c3bc3714370d9bfddb7c67099ca9d1f3
Margee Modi <S-Margee.Modi@lwtech.edu>
2021-04-29 18:40:32 -0700

    Connected databse and create child Controller

---
+++ Models/Emergency.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Blessings.Models
+{
+    public partial class Emergency
+    {
+        [Key]
+        [Column("emergency_id")]
+        public int EmergencyId { get; set; }
+        [Required]
+        [Column("emergencyContactFirstName")]
+        [StringLength(50)]
+        public string EmergencyContactFirstName { get; set; }
+        [Required]
+        [Column("emergencyContactLastName")]
+        [StringLength(50)]
+        public string EmergencyContactLastName { get; set; }
+        [Required]
+        [Column("emergencyContactPhone")]
+        [StringLength(10)]
+        public string EmergencyContactPhone { get; set; }
+        [StringLength(25)]
+        public string Relationship { get; set; }
+        [Column("child_id")]
+        public int ChildId { get; set; }
+
+        [ForeignKey(nameof(ChildId))]
+        [InverseProperty("Emergency")]
+        public virtual Child Child { get; set; }
+    }
+}
---
+++ Models/Medical.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Blessings.Models
+{
+    public partial class Medical
+    {
+        [Key]
+        [Column("medical_id")]
+        public int MedicalId { get; set; }
+        [Required]
+        [Column("personToContactFirstName")]
+        [StringLength(50)]
+        public string PersonToContactFirstName { get; set; }
+        [Required]
+        [Column("personToContactLastName")]
+        [StringLength(50)]
+        public string PersonToContactLastName { get; set; }
+        [Required]
+        [Column("personToContactPhone")]
+        [StringLength(10)]
+        public string PersonToContactPhone { get; set; }
+        [Required]
+        [Column("childsDoctorFirstName")]
+        [StringLength(50)]
+        public string ChildsDoctorFirstName { get; set; }
+        [Required]
+        [Column("childsDoctorLastName")]
+        [StringLength(50)]
+        public string ChildsDoctorLastName { get; set; }
+        [Required]
+        [Column("childsDoctorPhone")]
+        [StringLength(10)]
+        public string ChildsDoctorPhone { get; set; }
+        [Required]
+        [Column("regularlyUsedHospitalName")]
+        [StringLength(50)]
+        public string RegularlyUsedHospitalName { get; set; }
+        [Column("diaetryRestriction")]
+        [StringLength(100)]
+        public string DiaetryRestriction { get; set; }
+        [Column("medicalIssue")]
+        [StringLength(100)]
+        public string MedicalIssue { get; set; }
+        [Column("child_id")]
+        public int ChildId { get; set; }
+
+        [ForeignKey(nameof(ChildId))]
+        [InverseProperty("Medical")]
+        public virtual Child Child { get; set; }
+    }
+}
